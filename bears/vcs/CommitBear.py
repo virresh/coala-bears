f35ecbb40d11e3d3e3ce0e03bfe53458bf61ac15
Viresh Gupta <viresh16118@iiitd.ac.in>
2018-03-03 17:51:32 +0530

    HgCommitBear.py: Add Mercurial support for commits
    Add support for mercurial commits using a HgCommitBear
    on the same lines as GitCommitBear. This bear also uses
    nltk to inforce imperative mood check and checks the
    HEAD commit only. Requires Hg installed to function.
    
    Closes https://github.com/coala/coala-bears/issues/2286

---
+++ bears/vcs/CommitBear.py
@@ -0,0 +1,187 @@
+import nltk
+import re
+import abc
+
+from coalib.bears.GlobalBear import GlobalBear
+from dependency_management.requirements.PipRequirement import PipRequirement
+from coalib.results.Result import Result
+from coalib.settings.FunctionMetadata import FunctionMetadata
+from coalib.settings.Setting import typed_list
+
+
+class _CommitBear(GlobalBear):
+    __metaclass__ = abc.ABCMeta
+    REQUIREMENTS = {PipRequirement('nltk', '3.2')}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CONCATENATION_KEYWORDS = [r',', r'\sand\s']
+
+    _nltk_data_downloaded = False
+
+    def setup_dependencies(self):
+        if not self._nltk_data_downloaded and bool(
+                self.section.get('shortlog_imperative_check', True)):
+            nltk.download([
+                'punkt',
+                'averaged_perceptron_tagger',
+            ])
+            type(self)._nltk_data_downloaded = True
+
+    @abc.abstractmethod
+    def check_issue_reference():
+        pass
+
+    @classmethod
+    def get_shortlog_checks_metadata(cls):
+        return FunctionMetadata.from_function(
+            cls.check_shortlog,
+            omit={'self', 'shortlog'})
+
+    @classmethod
+    def get_body_checks_metadata(cls):
+        return FunctionMetadata.from_function(
+            cls.check_body,
+            omit={'self', 'body'})
+
+    @classmethod
+    def get_issue_checks_metadata(cls):
+        return FunctionMetadata.from_function(
+            cls.check_issue_reference,
+            omit={'self', 'body'})
+
+    @classmethod
+    def get_metadata(cls):
+        return FunctionMetadata.merge(
+            FunctionMetadata.from_function(
+                cls.run,
+                omit={'self', 'dependency_results'}),
+            cls.get_shortlog_checks_metadata(),
+            cls.get_body_checks_metadata(),
+            cls.get_issue_checks_metadata())
+
+    def check_shortlog(self, shortlog,
+                       shortlog_length: int=50,
+                       shortlog_regex: str='',
+                       shortlog_trailing_period: bool=None,
+                       shortlog_imperative_check: bool=True,
+                       shortlog_wip_check: bool=True):
+        """
+        Checks the given shortlog.
+
+        :param shortlog:                 The shortlog message string.
+        :param shortlog_length:          The maximum length of the shortlog.
+                                         The newline character at end does not
+                                         count to the length.
+        :param shortlog_regex:           A regex to check the shortlog with.
+        :param shortlog_trailing_period: Whether a dot shall be enforced at end
+                                         end or not (or ``None`` for "don't
+                                         care").
+        :param shortlog_wip_check:       Whether a "WIP" in the shortlog text
+                                         should yield a result or not.
+        """
+        diff = len(shortlog) - shortlog_length
+        if diff > 0:
+            yield Result(self,
+                         'Shortlog of the HEAD commit contains {} '
+                         'character(s). This is {} character(s) longer than '
+                         'the limit ({} > {}).'.format(
+                              len(shortlog), diff,
+                              len(shortlog), shortlog_length))
+
+        if (shortlog[-1] != '.') == shortlog_trailing_period:
+            yield Result(self,
+                         'Shortlog of HEAD commit contains no period at end.'
+                         if shortlog_trailing_period else
+                         'Shortlog of HEAD commit contains a period at end.')
+
+        if shortlog_regex:
+            match = re.fullmatch(shortlog_regex, shortlog)
+            if not match:
+                yield Result(
+                    self,
+                    'Shortlog of HEAD commit does not match given regex:'
+                    ' {regex}'.format(regex=shortlog_regex))
+
+        if shortlog_imperative_check:
+            colon_pos = shortlog.find(':')
+            shortlog = (shortlog[colon_pos + 1:]
+                        if colon_pos != -1
+                        else shortlog)
+            has_flaws = self.check_imperative(shortlog)
+            if has_flaws:
+                bad_word = has_flaws[0]
+                yield Result(self,
+                             "Shortlog of HEAD commit isn't in imperative "
+                             "mood! Bad words are '{}'".format(bad_word))
+        if shortlog_wip_check:
+            if 'wip' in shortlog.lower()[:4]:
+                yield Result(
+                    self,
+                    'This commit seems to be marked as work in progress and '
+                    'should not be used in production. Treat carefully.')
+
+    def check_imperative(self, paragraph):
+        """
+        Check the given sentence/s for Imperatives.
+
+        :param paragraph:
+            The input paragraph to be tested.
+        :return:
+            A list of tuples having 2 elements (invalid word, parts of speech)
+            or an empty list if no invalid words are found.
+        """
+        words = nltk.word_tokenize(nltk.sent_tokenize(paragraph)[0])
+        # VBZ : Verb, 3rd person singular present, like 'adds', 'writes'
+        #       etc
+        # VBD : Verb, Past tense , like 'added', 'wrote' etc
+        # VBG : Verb, Present participle, like 'adding', 'writing'
+        word, tag = nltk.pos_tag(['I'] + words)[1:2][0]
+        if(tag.startswith('VBZ') or
+           tag.startswith('VBD') or
+           tag.startswith('VBG') or
+           word.endswith('ing')):  # Handle special case for VBG
+            return (word, tag)
+        else:
+            return None
+
+    def check_body(self, body,
+                   body_line_length: int=72,
+                   force_body: bool=False,
+                   ignore_length_regex: typed_list(str)=(),
+                   body_regex: str=None):
+        """
+        Checks the given commit body.
+
+        :param body:                The body of the commit message of HEAD.
+        :param body_line_length:    The maximum line-length of the body. The
+                                    newline character at each line end does not
+                                    count to the length.
+        :param force_body:          Whether a body shall exist or not.
+        :param ignore_length_regex: Lines matching each of the regular
+                                    expressions in this list will be ignored.
+        :param body_regex:          If provided, checks the presence of regex
+                                    in the commit body.
+        """
+        if len(body) == 0:
+            if force_body:
+                yield Result(self, 'No commit message body at HEAD.')
+            return
+
+        if body[0] != '\n':
+            yield Result(self, 'No newline found between shortlog and body at '
+                               'HEAD commit. Please add one.')
+            return
+
+        if body_regex and not re.fullmatch(body_regex, body.strip()):
+            yield Result(self, 'No match found in commit message for the '
+                               'regular expression provided: %s' % body_regex)
+
+        body = body.splitlines()
+        ignore_regexes = [re.compile(regex) for regex in ignore_length_regex]
+        if any((len(line) > body_line_length and
+                not any(regex.search(line) for regex in ignore_regexes))
+               for line in body[1:]):
+            yield Result(self, 'Body of HEAD commit contains too long lines. '
+                               'Commit body lines should not exceed {} '
+                               'characters.'.format(body_line_length))
--- bears/vcs/mercurial/HgCommitBear.py
+++ bears/vcs/mercurial/HgCommitBear.py
@@ -1,56 +1,50 @@
 import re
 import shutil
 import os
-from urllib.parse import urlparse
 
+from urllib.parse import urlparse
 from coala_utils.ContextManagers import change_directory
 from coalib.misc.Shell import run_shell_command
 from coalib.results.Result import Result
 from bears.vcs.CommitBear import _CommitBear
 
 
-class GitCommitBear(_CommitBear):
-    LANGUAGES = {'Git'}
-    ASCIINEMA_URL = 'https://asciinema.org/a/e146c9739ojhr8396wedsvf0d'
-    CAN_DETECT = {'Formatting'}
+class HgCommitBear(_CommitBear):
+    LANGUAGES = {'Hg'}
     SUPPORTED_HOST_KEYWORD_REGEX = {
-        'github': (r'[Cc]lose[sd]?'
-                   r'|[Rr]esolve[sd]?'
-                   r'|[Ff]ix(?:e[sd])?'),
-        'gitlab': (r'[Cc]los(?:e[sd]?|ing)'
-                   r'|[Rr]esolv(?:e[sd]?|ing)'
-                   r'|[Ff]ix(?:e[sd]|ing)?')
+        'bitbucket': (r'[Cc]los(?:e[sd]?|ing)'
+                      r'|[Rr]esolv(?:e[sd]?|ing)'
+                      r'|[Ff]ix(?:e[sd]|ing)?')
     }
 
     @classmethod
     def check_prerequisites(cls):
-        if shutil.which('git') is None:
-            return 'git is not installed.'
+        if shutil.which('hg') is None:
+            return 'hg is not installed.'
         else:
             return True
 
     @staticmethod
     def get_host_from_remotes():
         """
-        Retrieve the first host from the list of git remotes.
+        Retrieve the first host from the list of mercurial remotes.
         """
-        remotes, _ = run_shell_command(
-            "git config --get-regex '^remote.*.url$'")
+        remotes, _ = run_shell_command('hg paths')
 
         remotes = [url.split()[-1] for url in remotes.splitlines()]
         if len(remotes) == 0:
             return None
 
         url = remotes[0]
-        if 'git@' in url:
-            netloc = re.findall(r'@(\S+):', url)[0]
+        if 'hg@' in url:
+            netloc = re.findall(r'@(\S+)\.', url)[0]
         else:
             netloc = urlparse(url)[1]
-        return netloc.split('.')[0]
+        return netloc.split('.')[0].split('@')[-1]
 
     def run(self, allow_empty_commit_message: bool = False, **kwargs):
         """
-        Check the current git commit message at HEAD.
+        Check the current mercurial commit message at HEAD.
 
         This bear ensures automatically that the shortlog and body do not
         exceed a given line-length and that a newline lies between them.
@@ -59,10 +53,11 @@ class GitCommitBear(_CommitBear):
                                            allowed or not.
         """
         with change_directory(self.get_config_dir() or os.getcwd()):
-            stdout, stderr = run_shell_command('git log -1 --pretty=%B')
+            stdout, stderr = run_shell_command(
+                'hg log -l 1 --template "{desc}"')
 
         if stderr:
-            self.err('git:', repr(stderr))
+            self.err('hg:', repr(stderr))
             return
 
         stdout = stdout.rstrip('\n')
@@ -96,16 +91,16 @@ class GitCommitBear(_CommitBear):
         :param body:
             Body of the commit message of HEAD.
         :param body_close_issue:
-            GitHub and GitLab support auto closing issues with
+            BitBucket support auto closing issues with
             commit messages. When enabled, this checks for matching keywords
-            in the commit body by retrieving host information from git
+            in the commit body by retrieving host information from mercurial
             configuration. By default, if none of ``body_close_issue_full_url``
             and ``body_close_issue_on_last_line`` are enabled, this checks for
             presence of short references like ``closes #213``.
             Otherwise behaves according to other chosen flags.
             More on keywords follows.
-            [GitHub](https://help.github.com/articles/closing-issues-via-commit-messages/)
-            [GitLab](https://docs.gitlab.com/ce/user/project/issues/automatic_issue_closing.html)
+            [BitBucket](https://confluence.atlassian.com/bitbucket/resolve-issues-
+            automatically-when-users-push-code-221451126.html)
         :param body_close_issue_full_url:
             Checks the presence of issue close reference with a full URL
             related to some issue. Works along with ``body_close_issue``.
@@ -121,7 +116,12 @@ class GitCommitBear(_CommitBear):
             return
 
         host = self.get_host_from_remotes()
+
+        if host is None:
+            yield Result(self, 'No host configured in path.')
+            return
         if host not in self.SUPPORTED_HOST_KEYWORD_REGEX:
+            yield Result(self, 'Un-supported host in path.')
             return
 
         if body_close_issue_on_last_line:
@@ -136,10 +136,10 @@ class GitCommitBear(_CommitBear):
         if body_close_issue_full_url:
             result_info = 'full issue'
             issue_ref_regex = (
-                r'https?://{}\S+/issues/(\S+)'.format(re.escape(host)))
+                r'https?://{}\S+/issues/(\S+)/\S+'.format(re.escape(host)))
         else:
             result_info = 'issue'
-            issue_ref_regex = r'(?:\w+/\w+)?#(\S+)'
+            issue_ref_regex = r'(?:\w+/\w+)?#(\S+)/?'
 
         concat_regex = '|'.join(kw for kw in self.CONCATENATION_KEYWORDS)
         compiled_joint_regex = re.compile(
@@ -147,7 +147,7 @@ class GitCommitBear(_CommitBear):
                                     # eg: fix, closes etc.
 
             r'((?:\S(?!{1}))*\S'    # match links/tags
-                                    # eg: fix #123, fix https://github.com
+                                    # eg: fix #123, fix https://bitbucket.com
 
             r'(?:\s*(?:{1})\s*'     # match conjunctions like ',','and'
 
